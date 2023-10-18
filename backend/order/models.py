4b676d0131fa5063f010b8f1b9352d6ab2c6af88
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2023-10-18 14:34:16 +0000

    latest

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from upload.models import Product 
+User = get_user_model()
 class Order(models.Model):
-    pass
+    user = models.ForeignKey(User, related_name="user_order", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="ordered_product", on_delete=models.DO_NOTHING)
+    ordered_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
