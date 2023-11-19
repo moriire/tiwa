0344a8ed1b8cd154454b161b8b3ae48bab3e92c8
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2023-11-19 13:55:57 +0100

    latest

--- backend/backend/settings/dev_settings.py
+++ backend/backend/settings/dev_settings.py
@@ -163,7 +163,7 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGIN = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_ALLOWED_ORIGINS = [
     get("ALLOWED_ORIGINS") 
