d7bbfa91b8285e5094a95dfa08440b7d0b81a4e4
IBM Abdulsalam <ibmabdulsalam@gmail.com>
2023-11-19 11:44:16 +0100

    latest

--- backend/backend/settings/dev_settings.py
+++ backend/backend/settings/dev_settings.py
@@ -161,11 +161,7 @@ MEDIA_ROOT = BASE_DIR /'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGIN = True
-CORS_ALLOWED_ORIGINS = [
-     "http://localhost:4173",
-     "http://localhost:5173",
-     "https://tiwa-frontend.onrender.com",
-]
+
 CSRF_ALLOWED_ORIGINS = [
     get("ALLOWED_ORIGINS") 
 ]
--- backend/backend/settings/prod_settings.py
+++ backend/backend/settings/prod_settings.py
@@ -167,7 +167,6 @@ MEDIA_ROOT = BASE_DIR /'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-     get("ALLOWED_ORIGINS"),
      "http://localhost:4173",
      "http://localhost:5173",
      "https://tiwa-frontend.onrender.com",
